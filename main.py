a5cc0cdf0dc7a9f793069586b29774772cf51d30
Teg <tegmultani@hotmail.com>
2023-08-05 18:04:46 -0700

    Added a call to get user stats

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+# TO DO
+# 1. CREATE CACHE HANDLER
+
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
+import json
 
-scope = "user-library-read"
+scope = "user-top-read"
 
 # SPOTIPY_CLIENT_ID='your-spotify-client-id'
 # SPOTIPY_CLIENT_SECRET='your-spotify-client-secret'
@@ -16,6 +21,17 @@ SPOTIPY_REDIRECT_URI = os.environ.get('SPOTIPY_REDIRECT_URI')
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 
+m_username = sp.current_user()['display_name']
 
+#  TOP ARTISTS / TOP TRACKS
+# 'short_term' (4 weeks)
+# 'medium_term' (6 months)
+# 'long_term' (several years / all time)
 
+m_top50_artists_4weeks = sp.current_user_top_artists(limit=50, time_range='short_term')
+m_top50_artists_6months = sp.current_user_top_artists(limit=50, time_range='medium_term')
+m_top50_artists_alltime = sp.current_user_top_artists(limit=50, time_range='long_term')
 
+m_top50_tracks_4weeks = sp.current_user_top_tracks(limit=50, time_range='short_term')
+m_top50_tracks_6months = sp.current_user_top_tracks(limit=50, time_range='medium_term')
+m_top50_tracks_alltime = sp.current_user_top_tracks(limit=50, time_range='long_term')
